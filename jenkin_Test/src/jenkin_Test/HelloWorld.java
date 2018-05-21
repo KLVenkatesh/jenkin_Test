f5c02ca865762c5fe99bec642332ef206bdd48a8
vklaksh <vklaksh@gmail.com>
2018-05-21 15:51:15 +0530

    Uploading new java file

---
+++ jenkin_Test/src/jenkin_Test/HelloWorld.java
@@ -0,0 +1,12 @@
+package jenkin_Test;
+
+import org.testng.annotations.Test;
+
+public class HelloWorld {
+
+	@Test
+	
+	public void test() {
+		System.out.println("Jenkin Test");
+	}
+}
--- jenkin_Test/src/jenkin_Test/MavenJenkinTest.java
+++ jenkin_Test/src/jenkin_Test/MavenJenkinTest.java
@@ -1,22 +1,13 @@
 package jenkin_Test;
 
-import org.openqa.selenium.WebDriver;
-import org.testng.Reporter;
 import org.testng.annotations.Test;
 
-import generic.BasePage;
-
-public class MavenJenkinTest extends BasePage {
-	
-	public MavenJenkinTest(WebDriver driver) {
-		super(driver);
-	}
+public class MavenJenkinTest {
 
 	@Test
 	public void testA() {
-		Reporter.log("Executing testA",true);
-		String etitle = driver.getTitle();
-		System.out.println(etitle);
+		System.out.println("TestA passed");
+		
 	}
 
 }
